2929ccb3b2dbe2562c1f67a74bfd7e51be8f030a
rajarya199 <aryalrajan078@gmail.com>
2026-01-13 00:44:47 +0545

    scroll effect

--- components/About.jsx
+++ components/About.jsx
@@ -4,17 +4,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import AboutImg from '../public/assets/about1.jpg';
 import { myinfo } from '@/constants/constant';
-
+import { ScrollReveal } from './ScrollReveal';
 const About = () => {
  
   return (
     <div id='about' className='w-full p-2 flex items-center py-16 '>
       <div className='max-w-[1240px] h-auto m-auto md:grid grid-cols-3 gap-8 mb-2 '>
         <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5] dark:text-[#818cf8]'>
+<ScrollReveal preset='heading'>
+   <p className='uppercase text-xl tracking-widest text-[#5651e5] dark:text-[#818cf8]'>
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
+          <h2 className='py-4 '>Who I Am</h2>
+</ScrollReveal>
+       
           <p className='py-2  text-gray-600 dark:text-[#ecf0f3] '>
             {myinfo.status},{myinfo.position}<br/>
             {myinfo.about1}
