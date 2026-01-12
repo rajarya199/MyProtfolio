6643d8a9f0d02b4fa4f49745ca11e1f4f95d94b8
rajarya199 <aryalrajan078@gmail.com>
2026-01-13 00:46:50 +0545

    framer motion-fade

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
+"use client"
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Education from "@/components/Education";
 import Main from "@/components/Main";
 import Project from "@/components/Project";
 import Skills from "@/components/Skills";
+import { motion } from "framer-motion";
 
 
 export default function Home() {
   return (
-<>
+<motion.main
+   initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+>
   <Main/>
   <About/>
   <Skills/>
@@ -17,6 +23,6 @@ export default function Home() {
   <Education/>
 
   <Contact/>
-</>
+</motion.main>
   );
 }
