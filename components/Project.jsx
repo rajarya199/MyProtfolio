ac3c70d7885d5ca65700e51b4fb95ab2dc02de4f
rajarya199 <aryalrajan078@gmail.com>
2026-01-13 00:43:02 +0545

    scroll effect

--- components/Project.jsx
+++ components/Project.jsx
@@ -2,15 +2,24 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import { ProjectsCard } from './ProjectsCard';
+import { ScrollReveal } from './ScrollReveal';
 
 const Project = () => {
   return (
     <div id='projects' className='w-full  mt-10 '>
       <div className='max-w-[1240px]  mx-auto  px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5] dark:text-[#818cf8]'>
+        <ScrollReveal preset='heading'>
+          <p className='text-xl tracking-widest uppercase text-[#5651e5] dark:text-[#818cf8]'>
           Projects
         </p>
-        <h2 className='py-4'>What I&apos;ve Built</h2>
+
+          </ScrollReveal>
+<ScrollReveal preset='subheading'>
+          <h2 className='py-4'>What I&apos;ve Built</h2>
+
+</ScrollReveal>
+        
+        
         <div >
             {/* <ProjectCard/> */}
             <ProjectsCard/>
